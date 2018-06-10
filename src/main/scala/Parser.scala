ab1c3e55618a7a82a1ff8e513779344a47ac272d
Natan Hoppe <nhoppe@gmail.com>
2018-06-10 10:24:41 -0700

    Finally managed to access SQLite database

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,8 +1,9 @@
 import scala.io.Source
+import scala.util.{Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.json.Json
-import slick.jdbc.SQLiteProfile.api._
 
-import models.SeenMalware
+import models._
 
 object Parser extends App {
   val dataSource = Source.fromFile("log.json")
@@ -11,6 +12,18 @@ object Parser extends App {
     line => Json.parse(line).as[SeenMalware]
   )
 
-  val database = Database.forConfig("sqlite")
-  database.close
+  val unknownMalwares = seenMalwares.filter(
+    _.disposition == Disposition.UNKNOWN
+  )
+
+  unknownMalwares.foreach { malware =>
+    SeenMalwaresDAO.findBySha(malware.sha256).onComplete {
+      case Success(result) => result match {
+        case Some(result) => println(result)
+        case None => ()
+      }
+
+      case Failure(e) => e.printStackTrace
+    }
+  }
 }
---
+++ src/main/scala/models/Disposition.scala
@@ -0,0 +1,5 @@
+object Disposition extends Enumeration {
+  val CLEAN = 1
+  val MALICIOUS = 2
+  val UNKNOWN = 3
+}
